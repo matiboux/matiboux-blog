367e988bc2a7898282262304d269789bb34ff2d7
Mathieu Guérin <mathieu.guerin@saegus.com>
2026-08-15 14:42:26 +0000

    Update i18n files

--- app/src/i18n/getLocaleByUrl.ts
+++ app/src/i18n/getLocaleByUrl.ts
@@ -2,13 +2,12 @@ import { getLocaleByPath } from 'astro:i18n'
 
 import { i18n as i18nConfig } from '~/config'
 
-const defaultLocale = i18nConfig.defaultLocale
-
-function getLocaleByUrl(url: URL | string, fallback?: true): string
-function getLocaleByUrl(url: URL | string, fallback: false): string | undefined
-function getLocaleByUrl(url: URL | string, fallback: boolean = true): string | undefined
+export function getLocaleByUrl(url: URL | string, fallback?: true): string
+export function getLocaleByUrl(url: URL | string, fallback: false): string | undefined
+export function getLocaleByUrl(url: URL | string, fallback: boolean = true): string | undefined
 {
 	const urlParts = typeof url === 'string' ? url.split('/') : url.pathname.split('/')
+
 	for (const part of urlParts)
 	{
 		if (!part)
@@ -16,14 +15,17 @@ function getLocaleByUrl(url: URL | string, fallback: boolean = true): string | u
 			continue
 		}
 
-		const locale = getLocaleByPath(part)
-		if (locale)
+		try
 		{
-			return locale
+			const locale = getLocaleByPath(part)
+			if (locale)
+			{
+				return locale
+			}
 		}
+		catch (error)
+		{}
 	}
 
-	return fallback ? defaultLocale : undefined
+	return fallback ? i18nConfig.defaultLocale : undefined
 }
-
-export default getLocaleByUrl
--- app/src/i18n/getUrlWithoutLocale.ts
+++ app/src/i18n/getUrlWithoutLocale.ts
@@ -1,6 +1,6 @@
 import { getLocaleByPath } from 'astro:i18n'
 
-function getUrlWithoutLocale(url: URL | string): string
+export function getUrlWithoutLocale(url: URL | string): string
 {
 	const urlPathnames = []
 	const urlParts = typeof url === 'string' ? url.split('/') : url.pathname.split('/')
@@ -8,26 +8,30 @@ function getUrlWithoutLocale(url: URL | string): string
 	let i = !urlParts[0] ? 1 : 0
 	while (i < urlParts.length)
 	{
-		if (urlParts[i])
+		try
 		{
-			const locale = getLocaleByPath(urlParts[i] as string)
-			if (locale)
+			if (urlParts[i])
 			{
-				i++
-				continue
+				const locale = getLocaleByPath(urlParts[i] as string)
+				if (locale)
+				{
+					++i
+					continue
+				}
 			}
 		}
+		catch (error)
+		{}
 
 		urlPathnames.push(urlParts[i])
-		i++
+		++i
 	}
+
 	while (i < urlParts.length)
 	{
 		urlPathnames.push(urlParts[i])
-		i++
+		++i
 	}
 
 	return '/' + urlPathnames.join('/')
 }
-
-export default getUrlWithoutLocale
