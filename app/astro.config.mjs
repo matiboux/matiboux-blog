c80117d600313cfa7bc84a4d8a52c45a3b509ba3
Mathieu Guérin <mathieu.guerin@saegus.com>
2026-08-15 14:43:45 +0000

    Update Astro configuration

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,21 +1,35 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
 import svelte from '@astrojs/svelte'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 import { i18n } from '/src/config'
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		server: {
-			watch: {
-				usePolling: true,
-			},
-		},
+	site: process.env.ASTRO_SITE_URL || undefined,
+	base: process.env.ASTRO_BASE_PATH || undefined,
+	build: {
+		assetsPrefix: process.env.ASTRO_ASSETS_PREFIX || undefined,
 	},
-	i18n: i18n,
 	integrations: [
 		svelte(),
-		tailwind(),
 	],
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+	},
+	i18n: i18n,
+	env: {
+		schema: {
+			// Deployment configuration
+			APP_ENV: envField.enum({ context: 'client', access: 'public', optional: true, values: ['dev', 'test', 'prod'], default: 'prod' }),
+			GITHUB_REPOSITORY_URL: envField.string({ context: 'client', access: 'public', optional: true }),
+			GITHUB_SHA: envField.string({ context: 'client', access: 'public', optional: true }),
+			VERSION_TAG: envField.string({ context: 'client', access: 'public', optional: true }),
+			// Application configuration
+			// Add env vars for your application here.
+		},
+		validateSecrets: true,
+	},
 })
