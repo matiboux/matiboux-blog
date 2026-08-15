791e6622753e51ebdcd9e4b23b91acdb03cf6451
Mathieu Guérin <mathieu.guerin@saegus.com>
2026-08-15 20:24:02 +0000

    Add feed.xml RSS feed

---
+++ app/src/pages/feed.xml.ts
@@ -0,0 +1,31 @@
+import type { APIContext } from 'astro'
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+
+import { defaultLocale, i18nFactory } from '~/i18n'
+import { site } from '~/site'
+
+export async function GET(context: APIContext)
+{
+	if (!context.site)
+	{
+		throw new Error('The `site` option must be set in the Astro config to generate the RSS feed.')
+	}
+
+	const posts = (await getCollection('posts'))
+		.sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
+
+	const _ = i18nFactory(defaultLocale)
+
+	return rss({
+		title: site.title ? _(site.title) : 'Matiboux Blog',
+		description: site.description ? _(site.description) : '',
+		site: context.site,
+		items: posts.map((post) => ({
+			title: post.data.title,
+			description: post.data.excerpt,
+			pubDate: post.data.date,
+			link: `/posts/${post.id}/`,
+		})),
+	})
+}
